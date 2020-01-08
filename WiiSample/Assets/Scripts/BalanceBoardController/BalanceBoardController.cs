d0551b48df1a536721be78fae54672a49f0c32d1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-01-08 20:43:41 +0900

    いろいろといれておく

---
+++ WiiSample/Assets/Scripts/BalanceBoardController/BalanceBoardController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceBoardController : SingletonBehaviour<BalanceBoardController>
+{
+    public int? connectingBalanceBoardIndex { get; private set; }
+
+    public bool IsRide { get; private set; }
+
+    // 総重量に対して指定した割合以上の差分がある場合、前後に進ませる
+    [SerializeField] private float TriggerThreasoldRate = 0.2f;
+    // この重量以下なら体重計に乗っていないと断定
+    [SerializeField] private float RideThreasoldWeight = 15f;
+
+    private float ResetGameTimeSecond = 0f;
+
+    void Start()
+    {
+        connectingBalanceBoardIndex = GetBalanceBoardConnectingIndex();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Wii.GetRemoteCount(); ++i)
+        {
+            if (Wii.IsActive(i) && Wii.GetExpType(i) == 3)
+            {
+                Vector4 theBalanceBoard = Wii.GetBalanceBoard(i);
+                Vector2 theCenter = Wii.GetCenterOfBalance(i);
+                float topWeight = theBalanceBoard.x + theBalanceBoard.y;
+                float bottomWeight = theBalanceBoard.z + theBalanceBoard.w;
+                float totalWeight = Wii.GetTotalWeight(i);
+                float topBottomDiff = topWeight - bottomWeight;
+                bool beforeIsRide = IsRide;
+                IsRide = totalWeight > RideThreasoldWeight;
+                if(IsRide){
+                    ResetGameTimeSecond = 0f;
+                    if (Mathf.Abs(topBottomDiff) > (totalWeight * TriggerThreasoldRate))
+                    {
+                        if(topBottomDiff > 0){
+                            TaikoController.Instance.GoForward(Mathf.Abs(topBottomDiff) / topWeight);
+                        }else{
+                            TaikoController.Instance.MoveBack(Mathf.Abs(topBottomDiff) / topWeight);
+                        }
+                    }
+                    else
+                    {
+                        TaikoController.Instance.StopTaiko();
+                    }
+                }
+                else if(beforeIsRide || ResetGameTimeSecond > 0f)
+                {
+                    ResetGameTimeSecond = ResetGameTimeSecond + Time.deltaTime;
+                }
+                break;
+            }
+        }
+        if(ResetGameTimeSecond > 60f)
+        {
+            TaikoController.Instance.ResetTaiko();
+        }
+    }
+
+    // WiiFitに接続している番号を保持して処理回数を減らす
+    public int? GetBalanceBoardConnectingIndex()
+    {
+        for (int i = 0; i < Wii.GetRemoteCount(); ++i)
+        {
+            // GetExpType == 3 がBaranceBoard
+            if (Wii.IsActive(i) && Wii.GetExpType(i) == 3)
+            {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    // WiiFitに接続しているかどうか判定
+    public bool IsBalanceBoardConnecting()
+    {
+        for (int i = 0; i < Wii.GetRemoteCount(); ++i)
+        {
+            // GetExpType == 3 がBaranceBoard
+            if (Wii.IsActive(i) && Wii.GetExpType(i) == 3)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // WiiFitのバッテリー残量
+    public float GetBalanceBoardBattery()
+    {
+        for (int i = 0; i < Wii.GetRemoteCount(); ++i)
+        {
+            // GetExpType == 3 がBaranceBoard
+            if (Wii.IsActive(i) && Wii.GetExpType(i) == 3)
+            {
+                return Wii.GetBattery(i);
+            }
+        }
+        return 0f;
+    }
+}
---
+++ WiiSample/Assets/Scripts/TaikoController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaikoController : SingletonBehaviour<TaikoController>
+{
+    [SerializeField] GameObject taikoObj;
+    // 移動量がこれ以上ならば太鼓の音がならない
+    [SerializeField] float playSEDistanceThreasold = 1f;
+    [SerializeField] float playSESpanSecond = 0.2f;
+    [SerializeField] int audioSourceLayerCount = 5;
+
+    private Animator taikoAnimator;
+    private Rigidbody taikoRigidbody;
+    private Vector3 prevPosition;
+    private Camera mainCamera;
+    private float timeCounter = 0f;
+    private bool isPlayLoop = false;
+    private List<AudioSource> seAudioList = new List<AudioSource>();
+    private Vector3 defaultPosition;
+    private Quaternion defaultRotation;
+
+    void Start()
+    {
+        defaultPosition = taikoObj.transform.position;
+        defaultRotation = taikoObj.transform.rotation;
+        mainCamera = Camera.main;
+        taikoAnimator = taikoObj.GetComponent<Animator>();
+        taikoRigidbody = taikoObj.GetComponent<Rigidbody>();
+        prevPosition = taikoObj.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diff = taikoObj.transform.position - prevPosition;
+        mainCamera.transform.position = mainCamera.transform.position + diff;
+        mainCamera.transform.LookAt(taikoObj.transform.position);
+        if (diff.magnitude > playSEDistanceThreasold)
+        {
+            if (!isPlayLoop)
+            {
+                isPlayLoop = true;
+                StopCoroutine(LoopPlaySE());
+                StartCoroutine(LoopPlaySE());
+            }
+        }
+        else
+        {
+            isPlayLoop = false;
+        }
+        prevPosition = taikoObj.transform.position;
+    }
+
+    private IEnumerator LoopPlaySE()
+    {
+        while (isPlayLoop)
+        {
+            if (seAudioList.Count >= audioSourceLayerCount)
+            {
+                AudioSource popAudio = seAudioList[0];
+                if (popAudio != null && popAudio.isPlaying)
+                {
+                    popAudio.Stop();
+                    StopCoroutine(SoundManager.Instance.SEAudioPlayCorutine(popAudio));
+                    Destroy(popAudio.gameObject);
+                    seAudioList.RemoveAt(0);
+                }
+            }
+            AudioSource seAudioSource = SoundManager.Instance.PlaySE("Drum_level3");
+            if (seAudioSource != null)
+            {
+                seAudioList.Add(seAudioSource);
+            }
+            yield return new WaitForSeconds(playSESpanSecond);
+        }
+    }
+
+    public void GoForward(float progress)
+    {
+        taikoAnimator.SetTrigger("Forward");
+        taikoRigidbody.velocity = taikoObj.transform.forward * progress * 5;
+    }
+
+    public void MoveBack(float progress)
+    {
+        taikoAnimator.SetTrigger("Back");
+        taikoRigidbody.velocity = -(taikoObj.transform.forward * progress * 5);
+    }
+
+    public void StopTaiko()
+    {
+        taikoAnimator.SetTrigger("Idle");
+    }
+
+    public void ResetTaiko()
+    {
+        taikoObj.transform.position = defaultPosition;
+        taikoObj.transform.rotation = defaultRotation;
+    }
+}
